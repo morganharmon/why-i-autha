fcdf9b87eacd4e567ef8d8b6a380484907992053
Morgan Harmon <morganharmon@gmail.com>
2022-05-16 11:33:47 -0700

    create new user and redirect to other page event

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { signupUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -6,6 +8,30 @@ const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
+
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+
+// 1. sign up event
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    console.log({ email: data.get('email'), password: data.get('password') });
+    const user = await signupUser(data.get('email'), data.get('password'));
+    // write signupUser function
+    if (user) {
+        location.replace('./other-page');
+    }
+
+
+});
+
+// 2. redirect to other-page if signed in
+
+// 3. redirect from other-page to home if not signed in
+
+// 4. sign in event
+
+// 5. log out event on other-page with redirect to home
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
 // Enter Supabase Information
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://yxfajitmasxbcohtbkqt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4ZmFqaXRtYXN4YmNvaHRia3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyODY4MTEsImV4cCI6MTk2Nzg2MjgxMX0.2L8306PS1qjxKhAvOhaxYHjp8eowvQquvK3yZw7Nyj8';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,12 +8,19 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({email, password});
+    if (response.user) {
+        return response.user;
+    } else {
+        console.error(response.error);
+    }
+}
 
-export async function signInUser(email, password) {}
+// export async function signInUser(email, password) {}
 
-export async function checkAuth() {}
+// export async function checkAuth() {}
 
-export async function redirectIfLoggedIn() {}
+// export async function redirectIfLoggedIn() {}
 
-export async function logout() {}
+// export async function logout() {}
